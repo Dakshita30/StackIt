201dd0f6180254eb49be63d893dd2b8485f68943
Dakshita Sandal <dakshita08783@vvdav.info>
2025-07-12 12:00:54 +0530

    loginscreen

--- stack_it/lib/main.dart
+++ stack_it/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stack_it/theme/Pallete.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: 'StackIt',
+      theme: Pallete.natureTheme,
+
       home: const SizedBox(),
     );
   }
---
+++ stack_it/lib/theme/Pallete.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color mutedGreen = Color(0xFF727D73);
+  static const Color softOlive = Color(0xFFAAB99A);
+  static const Color lightSage = Color(0xFFD0DDD0);
+  static const Color paleCream = Color(0xFFF0F0D7);
+
+  static final ThemeData natureTheme = ThemeData(
+    scaffoldBackgroundColor: paleCream,
+    primaryColor: mutedGreen,
+    cardColor: softOlive,
+
+    appBarTheme: const AppBarTheme(
+      backgroundColor: mutedGreen,
+      iconTheme: IconThemeData(color: paleCream),
+      titleTextStyle: TextStyle(
+        color: paleCream,
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.black87),
+      headlineSmall: TextStyle(color: Colors.black87),
+    ),
+    drawerTheme: const DrawerThemeData(backgroundColor: softOlive),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: mutedGreen,
+        foregroundColor: paleCream,
+      ),
+    ),
+  );
+}
