bdb6e4a98927abe1b89e52df685f0c9f22927a5f
Dakshita Sandal <dakshita08783@vvdav.info>
2025-07-12 14:08:49 +0530

    updating

--- stack_it/lib/theme/Pallete.dart
+++ stack_it/lib/theme/Pallete.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 class Pallete {
@@ -34,42 +33,3 @@ class Pallete {
     ),
   );
 }
-=======
-import 'package:flutter/material.dart';
-
-class Pallete {
-  static const Color mutedGreen = Color(0xFF727D73);
-  static const Color softOlive = Color(0xFFAAB99A);
-  static const Color lightSage = Color(0xFFD0DDD0);
-  static const Color paleCream = Color(0xFFF0F0D7);
-
-  static final ThemeData natureTheme = ThemeData(
-    scaffoldBackgroundColor: paleCream,
-    primaryColor: mutedGreen,
-    cardColor: softOlive,
-  
-    appBarTheme: const AppBarTheme(
-      backgroundColor: mutedGreen,
-      iconTheme: IconThemeData(color: paleCream),
-      titleTextStyle: TextStyle(
-        color: paleCream,
-        fontWeight: FontWeight.bold,
-        fontSize: 20,
-      ),
-    ),
-    textTheme: const TextTheme(
-      bodyMedium: TextStyle(color: Colors.black87),
-      headlineSmall: TextStyle(color: Colors.black87),
-    ),
-    drawerTheme: const DrawerThemeData(
-      backgroundColor: softOlive,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: mutedGreen,
-        foregroundColor: paleCream,
-      ),
-    ),
-  );
-}
->>>>>>> 16b6ccf8f48a2b38071d11823e54c9f4967e8145
